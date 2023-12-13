4625a74d92074823279744bdf86f68071cfd594e
EduardoAlparov <workman133@yandex.ru>
2023-12-13 10:07:09 +0300

    second day

--- js/modules/lazyload.js
+++ js/modules/lazyload.js
@@ -1,6 +1,24 @@
 import "../../libs/vanilla-lazyload/lazyload.min.js";
 
 export default () => {
+    if (canUseWebp() === false) {
+        const lazyBgItems = document.querySelectorAll('.lazy[data-bg-fallback]');
+
+        lazyBgItems.forEach((item) => {
+            const srcBgFallback = item.getAttribute('data-bg-fallback');
+            item.setAttribute('data-bg', srcBgFallback);
+        });
+    }
+
+    function canUseWebp() {
+        const elem = document.createElement('canvas');
+
+        if (elem.getContext && elem.getContext('2d')) {
+          return elem.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+        }
+        return false;
+    }
+
     function logElementEvent(eventName, element) {
         console.log(Date.now(), eventName, element.getAttribute("data-src"));
     }
